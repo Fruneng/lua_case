c2f6bdbb6da5ce53c587debac7d0073481172725
Fruneng <zunceng@gmail.com>
2015-05-05 10:29:38 +0800

    add gcc CFLAG -std=c++0x

--- cpp_object/lua_extern.hpp
+++ cpp_object/lua_extern.hpp
@@ -12,10 +12,10 @@ extern "C"{
 
 
 #define err_exit(num,fmt,args...)  \
-    do{printf("[%s:%d]"fmt"\n",__FILE__,__LINE__,##args);exit(num);} while(0)
+    do{printf("[%s:%d]" fmt "\n",__FILE__,__LINE__,##args);exit(num);} while(0)
     
 #define err_return(num,fmt,args...)  \
-    do{printf("[%s:%d]"fmt"\n",__FILE__,__LINE__,##args);return(num);} while(0)
+    do{printf("[%s:%d]" fmt "\n",__FILE__,__LINE__,##args);return(num);} while(0)
 
 
 #endif
